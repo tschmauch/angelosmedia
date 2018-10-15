42745d05a2f66b8b0ea36ba55284052f07d6c124
Tyler Schmauch <tylers@kornerstoneadmin.com>
2018-10-15 17:17:38 -0600

    remembering

---
+++ angelosmedia/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <p>Angelos Media: A Work in Progress</p>
+      </div>
+    );
+  }
+}
+
+export default App;
