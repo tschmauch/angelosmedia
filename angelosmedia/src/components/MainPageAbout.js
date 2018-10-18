06e73dafc358f835fe92afd6c266074bacfd4f06
Tyler Schmauch <tylers@kornerstoneadmin.com>
2018-10-18 15:35:21 -0600

    hover working

--- angelosmedia/src/components/MainPageAbout.js
+++ angelosmedia/src/components/MainPageAbout.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import CreateReactClass from 'create-react-class';
+// import axios from 'axios';
 
-function MainPageAbout() {
-  return (
-    <div id="mainPageAbout" className='mainPage col-sm-4 col-xs-12'><a className="mainLink" href="/"><h1>ABOUT</h1></a></div>
-  );
-}
+var MainPageAbout = CreateReactClass({
+  getInitialState: function () {
+    return {
+      hover: false
+    };
+  },
+  hoverOn: function () {
+    this.setState({ hover: true });
+  },
+  hoverOff: function () {
+    this.setState({ hover: false });
+  },
+  // async componentDidMount() {
+  //   const { match: { params } } = this.props;
+  //   const hover = (await axios.get(`http://localhost:8081/${params.questionId}`)).data;
+  //   this.setState({
+  //     hover,
+  //   });
+  // }
+  render: function () {
+    return (
+      <div
+        id="mainPageAbout"
+        className={ this.state.hover ? "hovering mainPage col-sm-4 col-xs-12" : "notHovering mainPage col-sm-4 col-xs-12" }
+          onMouseEnter={this.hoverOn}
+          onMouseLeave={this.hoverOff}>
+        <a href="/" >
+          <h1>ABOUT</h1>
+        </a>
+      </div>
+
+    )
+  }
+})
 
 export default MainPageAbout;
--- angelosmedia/src/components/MainPagePhoto.js
+++ angelosmedia/src/components/MainPagePhoto.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import CreateReactClass from 'create-react-class';
+// import axios from 'axios';
 
-function MainPagePhoto() {
-  return (
-    <div id="mainPagePhoto" className='mainPage col-sm-4 col-xs-12'><a className="mainLink" href="/"><h1>PHOTO</h1></a></div>
-  );
-}
+var MainPagePhoto = CreateReactClass({
+  getInitialState: function () {
+    return {
+      hover: false
+    };
+  },
+  hoverOn: function () {
+    this.setState({ hover: true });
+  },
+  hoverOff: function () {
+    this.setState({ hover: false });
+  },
+  // async componentDidMount() {
+  //   const { match: { params } } = this.props;
+  //   const hover = (await axios.get(`http://localhost:8081/${params.questionId}`)).data;
+  //   this.setState({
+  //     hover,
+  //   });
+  // }
+  render: function () {
+    return (
+      <div
+        id="mainPagePhoto"
+        className={ this.state.hover ? "hovering mainPage col-sm-4 col-xs-12" : "notHovering mainPage col-sm-4 col-xs-12" }
+          onMouseEnter={this.hoverOn}
+          onMouseLeave={this.hoverOff}>
+        <a href="/" >
+          <h1>PHOTO</h1>
+        </a>
+      </div>
 
+    )
+  }
+})
 export default MainPagePhoto;
