92e4628100aeae6c0c25cc28fbb205c6a1144430
Tyler Schmauch <tylers@kornerstoneadmin.com>
2018-10-19 16:17:10 -0600

    not much work today

--- angelosmedia/src/components/App.js
+++ angelosmedia/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 import NavBar from './NavBar';
 import MainPageVideo from './MainPageVideo';
 import MainPageAbout from './MainPageAbout';
